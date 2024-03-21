3ce92fc0f6681f2af35e2a5fa98c004f57693a0b
ZenDragonX <128883795+ZenDragonX@users.noreply.github.com>
2024-03-21 16:26:46 +0700

    fixed memory leak

--- Internal/AsynchronousSocketListener.cs
+++ Internal/AsynchronousSocketListener.cs
@@ -3,169 +3,171 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
 
-namespace rcon.Internal
+namespace rcon.Internal;
+
+internal class AsynchronousSocketListener(IPAddress ipAddress, int port)
 {
-    internal class AsynchronousSocketListener
+    internal delegate void MessageReceived(Socket socket, int requestId, PacketType type, string payload);
+    internal event MessageReceived? OnMessage;
+
+    // Create a TCP/IP socket.  
+    private readonly Socket _listener = new(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+    private readonly List<StateObject> _clients = [];
+    
+    internal void StartListening()
     {
-        internal delegate void MessageReceived(Socket socket, int requestId, PacketType type, string payload);
-        internal event MessageReceived OnMessage;
-        private Socket listener;
-
-        private List<StateObject> clients = new List<StateObject>();
-
-        internal void StartListening(int port)
+        // Bind the socket to the local endpoint and listen for incoming connections.  
+        try
         {
-            IPAddress ipAddress = IPAddress.Any;
-            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
+            var localEndPoint = new IPEndPoint(ipAddress, port);
 
-            // Create a TCP/IP socket.  
-            listener = new Socket(ipAddress.AddressFamily,
-                SocketType.Stream, ProtocolType.Tcp);
-
-            // Bind the socket to the local endpoint and listen for incoming connections.  
-            try
-            {
-                listener.Bind(localEndPoint);
-                listener.Listen(100);
-            }
-            catch (Exception e)
-            {
-                UnityEngine.Debug.LogError(e.ToString());
-            }
+            _listener.Bind(localEndPoint);
+            _listener.Listen(100);
+            
+            // start listening for the first client
+            _listener.BeginAccept(AcceptCallback, _listener);
         }
-
-        private bool isConnected(Socket c)
+        catch (Exception e)
         {
-            try
+            Debug.LogError(e.ToString());
+        }
+    }
+    
+    private bool IsConnected(Socket? c)
+    {
+        try
+        {
+            if (c is { Connected: true })
             {
-                if (c != null && c != null && c.Connected)
+                if (c.Poll(0, SelectMode.SelectRead))
                 {
-                    if (c.Poll(0, SelectMode.SelectRead))
-                    {
-                        return !(c.Receive(new byte[1], SocketFlags.Peek) == 0);
-                    }
-                    return true;
+                    return c.Receive(new byte[1], SocketFlags.Peek) != 0;
                 }
-                return false;
+                return true;
             }
-            catch
+            return false;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    internal void Update()
+    {
+
+        // copy the original list to an array to prevent errors when removing from the original list.
+        foreach (var state in _clients.ToArray()) 
+            if (!IsConnected(state.WorkSocket))
             {
-                return false;
+                Debug.Log("Rcon client disconnected");
+                state.WorkSocket?.Close();
+                _clients.Remove(state);
             }
+        
+        // memory leak when called inside the Update loop, instead call it after AcceptCallback
+        //_listener?.BeginAccept(AcceptCallback, _listener);
+    }
+
+    internal void AcceptCallback(IAsyncResult ar)
+    {
+        // Signal the main thread to continue.  
+        //allDone.Set();
+
+        // Get the socket that handles the client request.  
+        var listener = (Socket)ar.AsyncState;
+        var handler = listener.EndAccept(ar);
+
+        // Create the state object.  
+        var state = new StateObject
+        {
+            WorkSocket = handler
+        };
+        _clients.Add(state);
+        Debug.Log("Rcon client connected");
+        handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, ReadCallback, state);
+        
+        // start listening for the next client
+        _listener?.BeginAccept(AcceptCallback, _listener);
+    }
+
+    private void ReadCallback(IAsyncResult ar)
+    {
+        var content = string.Empty;
+
+        // Retrieve the state object and the handler socket  
+        // from the asynchronous state object.  
+        var state = (StateObject)ar.AsyncState;
+        var handler = state.WorkSocket;
+
+        if (handler == null)
+            return;
+
+        // Read data from the client socket.
+        int bytesRead = handler.EndReceive(ar);
+        int length = BitConverter.ToInt32(state.buffer, 0);
+        int requestId = BitConverter.ToInt32(state.buffer, sizeof(int));
+        int type = BitConverter.ToInt32(state.buffer, sizeof(int) * 2);
+        length -= sizeof(int) * 3 - 2;
+        byte[] payload = new byte[length];
+        for (var i = 0; i < length; i++)
+        {
+            payload[i] = state.buffer[(sizeof(int) * 3) + i];
         }
 
-        internal void Update()
-        {
+        OnMessage?.Invoke(handler, requestId, (PacketType)type, Encoding.ASCII.GetString(payload));
 
-            for (int i = 0; i < clients.Count; i++)
+        // read another packet probably?
+        state.buffer = new byte[StateObject.BufferSize]; // clear the buffer
+        handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, ReadCallback, state);
+    }
+
+    private void Send(Socket handler, string data)
+    {
+        // Convert the string data to byte data using ASCII encoding.  
+        byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+        // Begin sending the data to the remote device.  
+        handler.BeginSend(byteData, 0, byteData.Length, 0, SendCallback, handler);
+    }
+
+    private void SendCallback(IAsyncResult ar)
+    {
+        try
+        {
+            // Retrieve the socket from the state object.  
+            Socket handler = (Socket)ar.AsyncState;
+
+            // Complete sending the data to the remote device.  
+            int bytesSent = handler.EndSend(ar);
+            Debug.Log($"Sent {bytesSent} bytes to client.");
+
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e.ToString());
+        }
+    }
+
+    internal void Close()
+    {
+        if (_listener is { Connected: true })
+        {
+            foreach (var client in _clients)
             {
-                StateObject state = clients[i];
-                if (!isConnected(state.workSocket))
+                if (IsConnected(client.WorkSocket))
                 {
-                    UnityEngine.Debug.Log("Rcon client disconnected");
-                    state.workSocket.Close();
-                    clients.Remove(state);
+                    client.WorkSocket?.Close();
                 }
             }
-
-            listener.BeginAccept(
-                        new AsyncCallback(AcceptCallback),
-                        listener);
+            _listener.Close();
         }
 
-        internal void AcceptCallback(IAsyncResult ar)
-        {
-            // Signal the main thread to continue.  
-            //allDone.Set();
-
-            // Get the socket that handles the client request.  
-            Socket listener = (Socket)ar.AsyncState;
-            Socket handler = listener.EndAccept(ar);
-
-            // Create the state object.  
-            StateObject state = new StateObject();
-            state.workSocket = handler;
-            clients.Add(state);
-            UnityEngine.Debug.Log("Rcon client connected");
-            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
-                new AsyncCallback(ReadCallback), state);
-        }
-
-        private void ReadCallback(IAsyncResult ar)
-        {
-            string content = string.Empty;
-
-            // Retrieve the state object and the handler socket  
-            // from the asynchronous state object.  
-            StateObject state = (StateObject)ar.AsyncState;
-            Socket handler = state.workSocket;
-
-            // Read data from the client socket.
-            int bytesRead = handler.EndReceive(ar);
-            int length = BitConverter.ToInt32(state.buffer, 0);
-            int requestId = BitConverter.ToInt32(state.buffer, sizeof(int));
-            int type = BitConverter.ToInt32(state.buffer, sizeof(int) * 2);
-            length -= (sizeof(int) * 3) - 2;
-            byte[] payload = new byte[length];
-            for (int i = 0; i < length; i++)
-            {
-                payload[i] = state.buffer[(sizeof(int) * 3) + i];
-            }
-
-            OnMessage?.Invoke(handler, requestId, (PacketType)type, Encoding.ASCII.GetString(payload));
-
-            // read another packet probably?
-            state.buffer = new byte[StateObject.BufferSize]; // clear the buffer
-            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
-                new AsyncCallback(ReadCallback), state);
-        }
-
-        private void Send(Socket handler, string data)
-        {
-            // Convert the string data to byte data using ASCII encoding.  
-            byte[] byteData = Encoding.ASCII.GetBytes(data);
-
-            // Begin sending the data to the remote device.  
-            handler.BeginSend(byteData, 0, byteData.Length, 0,
-                new AsyncCallback(SendCallback), handler);
-        }
-
-        private void SendCallback(IAsyncResult ar)
-        {
-            try
-            {
-                // Retrieve the socket from the state object.  
-                Socket handler = (Socket)ar.AsyncState;
-
-                // Complete sending the data to the remote device.  
-                int bytesSent = handler.EndSend(ar);
-                UnityEngine.Debug.Log($"Sent {bytesSent} bytes to client.");
-
-                handler.Shutdown(SocketShutdown.Both);
-                handler.Close();
-
-            }
-            catch (Exception e)
-            {
-                UnityEngine.Debug.LogError(e.ToString());
-            }
-        }
-
-        internal void Close()
-        {
-            if (listener.Connected)
-            {
-                foreach (var client in clients)
-                {
-                    if (isConnected(client.workSocket))
-                    {
-                        client.workSocket.Close();
-                    }
-                }
-                listener.Close();
-            }
-
-        }
     }
 }
--- rcon.cs
+++ rcon.cs
@@ -4,168 +4,167 @@ using rcon.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text.RegularExpressions;
 
-namespace rcon
+namespace rcon;
+
+[BepInPlugin("nl.avii.plugins.rcon", "rcon", "1.0.2")]
+public class rcon : BaseUnityPlugin
 {
-    [BepInPlugin("nl.avii.plugins.rcon", "rcon", "1.0")]
-    public class rcon : BaseUnityPlugin
+    public delegate string UnknownCommand(string command, string[] args);
+    public event UnknownCommand? OnUnknownCommand;
+
+    public delegate string ParamsAction(params string[] args);
+    private AsynchronousSocketListener? _socketListener;
+
+    private readonly ConfigEntry<bool> _enabled;
+    private readonly ConfigEntry<int> _port;
+    private readonly ConfigEntry<string> _password;
+
+    private readonly Dictionary<string, Type> _commands = new();
+    private readonly Dictionary<string, ParamsAction> _customCommands = new();
+
+    private readonly Dictionary<string, BaseUnityPlugin> _owners = new();
+
+    private rcon()
     {
-        public delegate string UnknownCommand(string command, string[] args);
-        public event UnknownCommand OnUnknownCommand;
+        _enabled = Config.Bind("rcon", "enabled", false, "Enable RCON Communication");
+        _port = Config.Bind("rcon", "port", 2458, "Port to use for RCON Communication");
+        _password = Config.Bind("rcon", "password", "ChangeMe", "Password to use for RCON Communication");
+    }
 
-        public delegate string ParamsAction(params object[] args);
-        private AsynchronousSocketListener socketListener;
+    private void OnEnable()
+    {
+        if (!_enabled.Value) return;
+        _socketListener = new AsynchronousSocketListener(IPAddress.Any, _port.Value);
+        _socketListener.OnMessage += SocketListener_OnMessage;
+        _socketListener.StartListening();
+        
+        Logger.LogInfo("RCON Listening on port: " + _port.Value);
+    }
 
-        private ConfigEntry<bool> Enabled;
-        private ConfigEntry<int> Port;
-        private ConfigEntry<string> Password;
-
-        private Dictionary<string, Type> commands = new Dictionary<string, Type>();
-        private Dictionary<string, ParamsAction> customCommands = new Dictionary<string, ParamsAction>();
-
-        private Dictionary<string, BaseUnityPlugin> owners = new Dictionary<string, BaseUnityPlugin>();
-
-        private rcon()
+    private void SocketListener_OnMessage(Socket socket, int requestId, PacketType type, string payload)
+    {
+        switch (type)
         {
-            Enabled = Config.Bind("rcon", "enabled", false, "Enable RCON Communication");
-            Port = Config.Bind("rcon", "port", 2458, "Port to use for RCON Communication");
-            Password = Config.Bind("rcon", "password", "ChangeMe", "Password to use for RCON Communication");
+            case PacketType.Login:
+
+                var responsePayload = "Login Success";
+                if (payload.Trim() != _password.Value.Trim())
+                {
+                    responsePayload = "Login Failed";
+                    requestId = -1;
+                }
+
+                byte[] packet = PacketBuilder.CreatePacket(requestId, PacketType.LoginResponse, responsePayload);
+
+                socket.Send(packet);
+                break;
+            case PacketType.Command:
+                // strip slash if present
+                if (payload[0] == '/')
+                {
+                    payload = payload.Substring(1);
+                }
+
+                var data = Regex.Matches(payload, @"(?<=[ ][\""]|^[\""])[^\""]+(?=[\""][ ]|[\""]$)|(?<=[ ]|^)[^\"" ]+(?=[ ]|$)")
+                    .Cast<Match>()
+                    .Select(m => m.Value)
+                    .ToList();
+
+                string command = data[0].ToLower();
+                data.RemoveAt(0);
+
+                if (!_commands.ContainsKey(command) && !_customCommands.ContainsKey(command))
+                {
+                    var ret = OnUnknownCommand?.Invoke(command, data.ToArray()) ?? string.Empty;
+                    var cmd = PacketType.Command;
+                    if (ret.ToLower().Contains("unknown"))
+                        cmd = PacketType.Error;
+
+                    socket.Send(PacketBuilder.CreatePacket(requestId, cmd, ret));
+                    return;
+                }
+
+                if (_commands.TryGetValue(command, out var t))
+                {
+                    var instance = (ICommand) Activator.CreateInstance(t);
+                    instance.setOwner(_owners[command]);
+                    var response = instance.onCommand(data.ToArray());
+                    socket.Send(PacketBuilder.CreatePacket(requestId, type, response));
+                }
+                else if (_customCommands.TryGetValue(command, out var customCommand))
+                {
+                    var response = customCommand(data.ToArray());
+                    socket.Send(PacketBuilder.CreatePacket(requestId, type, response));
+                }
+                break;
+            default:
+                Logger.LogError($"Unknown packet type: {type}");
+                break;
+        }
+    }
+
+    private void Update()
+    {
+        if (!_enabled.Value) return;
+        if (_socketListener == null) return;
+
+        _socketListener.Update();
+    }
+
+    private void OnDisable()
+    {
+        if (!_enabled.Value) return;
+        _socketListener?.Close();
+    }
+
+    public void RegisterCommand<T>(BaseUnityPlugin owner, string command) where T : AbstractCommand, new()
+    {
+        command = command.ToLower();
+        if (_owners.ContainsKey(command))
+        {
+            Logger.LogError($"{command} already registered");
+            return;
+        }
+        _owners[command] = owner;
+        _commands[command] = typeof(T);
+        Logger.LogInfo($"Registering Command: {command}");
+    }
+
+    public void RegisterCommand(BaseUnityPlugin owner, string command, ParamsAction action)
+    {
+        command = command.ToLower();
+        if (_owners.ContainsKey(command))
+        {
+            Logger.LogError($"{command} already registered");
+            return;
+        }
+        _owners[command] = owner;
+        _customCommands[command] = action;
+        Logger.LogInfo($"Registering Command: {command}");
+    }
+
+    public void UnRegisterCommand(BaseUnityPlugin owner, string command)
+    {
+        if (!_owners.ContainsKey(command))
+        {
+            return;
         }
 
-        private void OnEnable()
+        if (_owners[command] != owner)
         {
-            if (!Enabled.Value) return;
-            socketListener = new AsynchronousSocketListener();
-            socketListener.OnMessage += SocketListener_OnMessage;
-
-            Logger.LogInfo("RCON Listening on port: " + Port.Value);
-            socketListener.StartListening(Port.Value);
+            return;
         }
 
-        private void SocketListener_OnMessage(Socket socket, int requestId, PacketType type, string payload)
-        {
-            switch (type)
-            {
-                case PacketType.Login:
+        _owners.Remove(command);
 
-                    string response_payload = "Login Success";
-                    if (payload.Trim() != Password.Value.Trim())
-                    {
-                        response_payload = "Login Failed";
-                        requestId = -1;
-                    }
+        if (_commands.ContainsKey(command))
+            _commands.Remove(command);
 
-                    byte[] packet = PacketBuilder.CreatePacket(requestId, PacketType.LoginResponse, response_payload);
-
-                    socket.Send(packet);
-                    break;
-                case PacketType.Command:
-                    // strip slash if present
-                    if (payload[0] == '/')
-                    {
-                        payload = payload.Substring(1);
-                    }
-
-                    var data = Regex.Matches(payload, @"(?<=[ ][\""]|^[\""])[^\""]+(?=[\""][ ]|[\""]$)|(?<=[ ]|^)[^\"" ]+(?=[ ]|$)")
-                        .Cast<Match>()
-                        .Select(m => m.Value)
-                        .ToList();
-
-                    string command = data[0].ToLower();
-                    data.RemoveAt(0);
-
-                    if (!commands.ContainsKey(command) && !customCommands.ContainsKey(command))
-                    {
-                        var ret = OnUnknownCommand?.Invoke(command, data.ToArray());
-                        PacketType t = PacketType.Command;
-                        if (ret.ToLower().Contains("unknown"))
-                            t = PacketType.Error;
-
-                        socket.Send(PacketBuilder.CreatePacket(requestId, t, ret));
-                        return;
-                    }
-
-                    if (commands.ContainsKey(command))
-                    {
-                        var t = commands[command];
-                        var instance = (ICommand)Activator.CreateInstance(t);
-                        instance.setOwner(owners[command]);
-                        var response = instance.onCommand(data.ToArray());
-                        socket.Send(PacketBuilder.CreatePacket(requestId, type, response));
-                    }
-                    else if (customCommands.ContainsKey(command))
-                    {
-                        var response = customCommands[command](data.ToArray());
-                        socket.Send(PacketBuilder.CreatePacket(requestId, type, response));
-                    }
-                    break;
-                default:
-                    Logger.LogError($"Unknown packet type: {type}");
-                    break;
-            }
-        }
-
-        private void Update()
-        {
-            if (!Enabled.Value) return;
-            if (socketListener == null) return;
-
-            socketListener.Update();
-        }
-
-        private void OnDisable()
-        {
-            if (!Enabled.Value) return;
-            socketListener.Close();
-        }
-
-        public void RegisterCommand<T>(BaseUnityPlugin owner, string command) where T : AbstractCommand, new()
-        {
-            command = command.ToLower();
-            if (owners.ContainsKey(command))
-            {
-                Logger.LogError($"{command} already registered");
-                return;
-            }
-            owners[command] = owner;
-            commands[command] = typeof(T);
-            Logger.LogInfo($"Registering Command: {command}");
-        }
-
-        public void RegisterCommand(BaseUnityPlugin owner, string command, ParamsAction action)
-        {
-            command = command.ToLower();
-            if (owners.ContainsKey(command))
-            {
-                Logger.LogError($"{command} already registered");
-                return;
-            }
-            owners[command] = owner;
-            customCommands[command] = action;
-            Logger.LogInfo($"Registering Command: {command}");
-        }
-
-        public void UnRegisterCommand(BaseUnityPlugin owner, string command)
-        {
-            if (!owners.ContainsKey(command))
-            {
-                return;
-            }
-
-            if (owners[command] != owner)
-            {
-                return;
-            }
-
-            owners.Remove(command);
-
-            if (commands.ContainsKey(command))
-                commands.Remove(command);
-
-            if (customCommands.ContainsKey(command))
-                customCommands.Remove(command);
-        }
+        if (_customCommands.ContainsKey(command))
+            _customCommands.Remove(command);
     }
 }
